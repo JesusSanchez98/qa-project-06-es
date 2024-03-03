0b71ee984065829140f15e14b2dca76c2f43cdc0
JesusSanchez98 <jesusbeto007@gmail.com>
2024-03-03 17:03:44 -0600

    fsfc

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE ="https://cnt-4847601f-3313-4db6-8bb1-4db7c223ffd1.containerhub.tripleten-services.com"
+URL_SERVICE ="https://cnt-7f814a66-a12a-4059-8a0f-fde85d572377.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 CREATE_KIT ="/api/v1/kits"
 USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
